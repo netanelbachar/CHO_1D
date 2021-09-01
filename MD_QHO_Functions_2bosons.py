192f39218bcc1c54ae945702596bd428d60901af
netanelbachar <netanel410@gmail.com>
2021-09-01 15:14:28 +0300

    Changing all the physics

--- MD_QHO_Functions_2bosons.py
+++ MD_QHO_Functions_2bosons.py
@@ -7,7 +7,7 @@ mass, w, hbar, kboltz = 1, 1, 1, 1
 beta = 10
 
 N_particles = 2
-# beads = 32
+beads = 1
 
 # Time
 T_max = 2000
@@ -23,8 +23,21 @@ block_size = int((g_steps-cutoff)/num_blocks)
 
 seed = 347885
 np.random.seed(seed)
-# wp = math.sqrt(beads) / (beta * hbar)
-# wp_long = math.sqrt(beads * N_particles) / (beta * hbar)
+wp = math.sqrt(beads) / (beta * hbar)
+
+
+def block_averaging(cutoff, block_size, data):
+    data_cut = data[cutoff:]
+    data_len = len(data_cut)
+    number_of_blocks = int(data_len/block_size)
+    average_array = np.zeros(number_of_blocks)
+    for i in range(number_of_blocks):
+        average_array[i] = (data_cut[i * block_size:(i + 1) * block_size]).mean()
+    averages = average_array.mean()
+    stdv = np.std(average_array, ddof=1) / np.sqrt(number_of_blocks)
+
+    return number_of_blocks, averages, stdv
+
 
 def initial_position(mass, w, hbar, N_particles, beads):
     mu, sigma = 0, math.sqrt(hbar / (mass * w))
@@ -36,24 +49,6 @@ def initial_velocity(mass, beta, N_particles, beads):
     mu, sigma = 0, math.sqrt(1 / (beta * mass))
     vx = np.random.normal(mu, sigma, N_particles * beads)
     return vx
-
-
-def oscillator_force(mass, w, beads, x):
-    f1 = - mass * w ** 2 * x / beads
-    return f1
-
-
-def ring_springs_force(mass, wp, x):
-    c2 = - mass * wp ** 2
-    f2 = np.zeros(len(x))
-    for j in range(len(x)):
-        if j == (len(x) - 1):
-            f2[j] = c2 * (2 * x[j] - x[0] - x[j - 1])
-        elif j == 0:
-            f2[j] = c2 * (2 * x[j] - x[j + 1] - x[-1])
-        else:
-            f2[j] = c2 * (2 * x[j] - x[j + 1] - x[j - 1])
-    return f2
 
 
 def oscillator_potential(mass, w, x):
@@ -69,6 +64,24 @@ def springs_potential(mass, wp, x):
         else:
             potential += 0.5 * mass * wp ** 2 * (x[j + 1] - x[j]) ** 2
     return potential
+
+
+def oscillator_force(mass, w, x):
+    f1 = - mass * w ** 2 * x
+    return f1
+
+
+def ring_springs_force(mass, wp, x):
+    c2 = - mass * wp ** 2
+    f2 = np.zeros(len(x))
+    for j in range(len(x)):
+        if j == (len(x) - 1):
+            f2[j] = c2 * (2 * x[j] - x[0] - x[j - 1])
+        elif j == 0:
+            f2[j] = c2 * (2 * x[j] - x[j + 1] - x[-1])
+        else:
+            f2[j] = c2 * (2 * x[j] - x[j + 1] - x[j - 1])
+    return f2
 
 
 def kinetic_1d(mass, v):
@@ -113,39 +126,31 @@ def kinetic_estimator_virial(beads, N_particles, mass, w, x):
     return k_estimator
 
 
-def kinetic_estimator_virial2(beads, beta, v_o, v_oo):
-    k_estimator = (beads / beta) + (((v_oo * np.exp(- beta * v_oo)) + (v_o *
-                                  np.exp(- beta * v_o))) / (np.exp(- beta * v_oo) + np.exp(- beta * v_o)))
+def kinetic_estimator_virial2(beads, x, f_ext):
+    k_estimator = ((1 / (2*beads)) * (x[:beads] * f_ext[:beads] + x[beads:] * f_ext[beads:])).sum()
     return k_estimator
 
 
-def force_comulative(mass, w, wp, wp_long, beta, beads, N_particles, x_long):
+def force_comulative(mass, wp, beta, beads, N_particles, x_long):
     x = np.array(np.split(x_long, N_particles))
     # Potential
-    v_o = springs_potential(mass, wp_long, x_long) + oscillator_potential(mass, w, x_long) / (beads * N_particles)
-    v_oo = springs_potential(mass, wp, x[0]) + oscillator_potential(mass, w, x[0]) / beads + \
-           springs_potential(mass, wp, x[1]) + oscillator_potential(mass, w, x[1]) / beads
+    v_o = springs_potential(mass, wp, x_long)  # One long ring
+    v_oo = springs_potential(mass, wp, x[0]) + springs_potential(mass, wp, x[1])  # Two rings
     exp_v_o = np.exp(- beta * v_o)
     exp_v_oo = np.exp(- beta * v_oo)
     z_exp = exp_v_oo + exp_v_o
     # Forces
-    f_oo_1 = (ring_springs_force(mass, wp, x[0]) + oscillator_force(mass, w, beads, x[0])) * exp_v_oo
-    f_oo_2 = (ring_springs_force(mass, wp, x[1]) + oscillator_force(mass, w, beads, x[1])) * exp_v_oo
-    f_o = (ring_springs_force(mass, wp_long, x_long) + oscillator_force(mass, w, beads, x_long)) * exp_v_o
+    f_oo_1 = (ring_springs_force(mass, wp, x[0])) * exp_v_oo
+    f_oo_2 = (ring_springs_force(mass, wp, x[1])) * exp_v_oo
+    f_o = (ring_springs_force(mass, wp, x_long)) * exp_v_o
     # Comulative Avg Force
-    f = np.zeros(beads * N_particles)
-    for bid in range(0, beads * N_particles):
-        if bid < beads:
-            f[bid] = (f_o[bid] + f_oo_1[bid]) / z_exp
-        else:
-            f[bid] = (f_o[bid] + f_oo_2[bid-beads]) / z_exp
+    f1 = (f_o[:beads] + f_oo_1) / z_exp
+    f2 = (f_o[beads:] + f_oo_2) / z_exp
+    f = np.concatenate((f1, f2))
     return f, z_exp, v_o, v_oo
 
 
-def langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, wp_long, beads, N_particles):
-    x = initial_position(mass, w, hbar, N_particles, beads)
-    vx = initial_velocity(mass, beta, N_particles, beads)
-    force, z_exp, v_o, v_oo = force_comulative(mass, w, wp, wp_long, beta, beads, N_particles, x)
+def langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, beads, N_particles):
     t = 0
     s = 0
     times = np.zeros(g_steps)
@@ -159,26 +164,28 @@ def langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, wp_long, bea
     temp_exp = np.zeros(g_steps)
     pot_est = np.zeros(g_steps)
     kin_est = np.zeros(g_steps)
-
     # Histogram
     pos = np.zeros(g_steps)
     vel = np.zeros(g_steps)
+
+    x = initial_position(mass, w, hbar, N_particles, beads)
+    vx = initial_velocity(mass, beta, N_particles, beads)
+    f_weighted, z_exp, v_o, v_oo = force_comulative(mass, wp, beta, beads, N_particles, x)
+    f_ext = oscillator_force(mass, w, x) / beads
+    force = f_weighted + f_ext
     ethermo = 0.0
     for step in range(0, g_steps):
         times[step] = t
         steps[step] = int(s)
         kin[step] = kinetic_1d(mass, vx)
-        potential[step] = - (1 / beta) * np.log(z_exp)
+        potential[step] = - (1 / beta) * np.log(z_exp/2) + oscillator_potential(mass, w, x) / beads
         e_tot[step] = kin[step] + potential[step]
-        e_change[step] = (e_tot[step] - e_tot[0]) * 100 / e_tot[0] # abs?
+        e_change[step] = abs((e_tot[step] - e_tot[0])) * 100 / e_tot[0]  # abs?
         h_eff[step] = e_tot[step] + ethermo
         h_eff_change[step] = abs((h_eff[step] - h_eff[0]) * 100) / h_eff[0]
         temp_exp[step] = 2.0 * kin[step] / (kboltz * beads * N_particles)  # Divided by beads from the Equipartition Function
-        pot_est[step] = - (1 / beta) * np.log(z_exp)
-        # pot_est[step] = oscillator_potential(mass, w, x) / (beads * N_particles)
-        # kin_est[step] = kinetic_estimator(beta, beads, N_particles, mass, wp_long, x)
-        # kin_est[step] = kinetic_estimator_virial(beads, N_particles, mass, w, x)
-        kin_est[step] = kinetic_estimator_virial2(beads, beta, v_o, v_oo)
+        pot_est[step] = oscillator_potential(mass, w, x) / beads
+        kin_est[step] = kinetic_estimator_virial2(beads, x, f_ext)
         t += dt
         s += 1
         # Histogram
@@ -186,29 +193,15 @@ def langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, wp_long, bea
         # vel[step] = vx[0]
 
         # Time propagation
-        vx, kin1, kin2 = langevin(mass, beta, vx, dt, beads, N_particles)
-        ethermo += kin1 - kin2
+        # vx, kin1, kin2 = langevin(mass, beta, vx, dt, beads, N_particles)
+        # ethermo += kin1 - kin2
         vx = vx + 0.5 * dt * (force / mass)
         x = x + vx * dt
-        force, z_exp, v_o, v_oo = force_comulative(mass, w, wp, wp_long, beta, beads, N_particles, x)
+        f_weighted, z_exp, v_o, v_oo = force_comulative(mass, wp, beta, beads, N_particles, x)
+        f_ext = oscillator_force(mass, w, x) / beads
+        force = f_weighted + f_ext
         vx = vx + 0.5 * dt * (force / mass)
-        vx, kin1, kin2 = langevin(mass, beta, vx, dt, beads, N_particles)
-        ethermo += kin1 - kin2
+        # vx, kin1, kin2 = langevin(mass, beta, vx, dt, beads, N_particles)
+        # ethermo += kin1 - kin2
     return steps, times, pos, vel, kin, potential, e_tot, e_change, temp_exp, pot_est, kin_est, h_eff_change
-
-
-
-def block_averaging(cutoff, block_size, data):
-    data_cut = data[cutoff:]
-    data_len = len(data_cut)
-    number_of_blocks = int(data_len/block_size)
-    average_array = np.zeros(number_of_blocks)
-    for i in range(number_of_blocks):
-        average_array[i] = (data_cut[i * block_size:(i + 1) * block_size]).mean()
-    averages = average_array.mean()
-    stdv = np.std(average_array, ddof=1) / np.sqrt(number_of_blocks)
-
-    return number_of_blocks, averages, stdv
-
-
 
--- PIMD_2bosons.py
+++ PIMD_2bosons.py
@@ -1,70 +1,72 @@
 from MD_QHO_Functions_2bosons import *
 
 
-# start = time.time()
-# steps, times, pos, vel, kin, potential, e_tot, e_change, temp_exp, pot_est, kin_est, h_eff_change = \
-#     langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, wp_long, beads, N_particles)
-# stop = time.time()
-# duration = stop - start
-# print("Time of execution:", duration)
-# number_of_blocks, avg_temp_exp, stdv_temp = block_averaging(cutoff, block_size=1000, data=temp_exp)
-# number_of_blocks1, avg_potential_est, stdv_potential = block_averaging(cutoff, block_size=4000, data=pot_est)
-# number_of_blocks2, avg_kin_est, stdv_kin = block_averaging(cutoff, block_size=4000, data=kin_est)
+start = time.time()
+steps, times, pos, vel, kin, potential, e_tot, e_change, temp_exp, pot_est, kin_est, h_eff_change = \
+    langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, beads, N_particles)
+stop = time.time()
+duration = stop - start
+print("Time of execution:", duration)
 
-beads_array = np.array([55])
-e_tot_array = np.zeros(len(beads_array))
-e_tot_stdv_array = np.zeros(len(beads_array))
-for i, b in enumerate(beads_array):
-    wp = math.sqrt(b) / (beta * hbar)
-    wp_long = math.sqrt(b * N_particles) / (beta * hbar)
+number_of_blocks, avg_temp_exp, stdv_temp = block_averaging(cutoff, block_size=1000, data=temp_exp)
+number_of_blocks1, avg_potential_est, stdv_potential = block_averaging(cutoff, block_size=4000, data=pot_est)
+number_of_blocks2, avg_kin_est, stdv_kin = block_averaging(cutoff, block_size=4000, data=kin_est)
 
-    start = time.time()
-    steps, times, pos, vel, kin, potential, e_tot, e_change, temp_exp, pot_est, kin_est, h_eff_change = \
-        langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, wp_long, b, N_particles)
-    stop = time.time()
-    duration = stop - start
-    print("Time of execution:", duration)
-
-    number_of_blocks, avg_temp_exp, stdv_temp = block_averaging(cutoff, block_size=1000, data=temp_exp)
-    number_of_blocks1, avg_potential_est, stdv_potential = block_averaging(cutoff, block_size=4000, data=pot_est)
-    number_of_blocks2, avg_kin_est, stdv_kin = block_averaging(cutoff, block_size=4000, data=kin_est)
-
-    print("Mean classical Temperature:", avg_temp_exp, "+-", stdv_temp)
-    print("Mean Potential_estimator:", avg_potential_est, "+-", stdv_potential)
-    print("Mean Kinetic_estimator:", avg_kin_est, "+-", stdv_kin)
-    print("Mean Total Energy Estimator:", avg_potential_est + avg_kin_est)
-
-    e_tot_array[i] = avg_kin_est + avg_potential_est
-    e_tot_stdv_array[i] = np.sqrt(stdv_potential**2 + stdv_kin**2)
-
-print ("e array:", e_tot_array)
-
-figper = plt.figure()
-plt.rcParams.update({'font.size': 13})
-plt.plot(beads_array, e_tot_array, '.', label="total energy", color="red")
-plt.xlabel("beads")
-plt.ylabel("Total Energy")
-plt.legend()
-plt.show()
-
-# figenergy = plt.figure()
-# plt.rcParams.update({'font.size': 13})
-# plt.plot(times, e_tot, '.', label="Total Energy vs Time", color="black")
-# plt.plot(times, kin, '.', label="Kinetic vs Time", color="red")
-# plt.plot(times, potential, '.', label="Potential vs Time", color="blue")
-# plt.xlabel("time")
-# plt.ylabel("Energy")
-# plt.legend()
-# plt.show()
+# beads_array = np.array([14, 18, 24, 28, 32, 36, 40, 46])
+# e_tot_array = np.zeros(len(beads_array))
+# e_tot_stdv_array = np.zeros(len(beads_array))
+# for i, b in enumerate(beads_array):
+#     wp = math.sqrt(b) / (beta * hbar)
+#
+#     start = time.time()
+#     steps, times, pos, vel, kin, potential, e_tot, e_change, temp_exp, pot_est, kin_est, h_eff_change = \
+#     langevin_dynamics(g_steps, dt, mass, beta, hbar, kboltz, w, wp, beads, N_particles)
+#     stop = time.time()
+#     duration = stop - start
+#     print("Time of execution:", duration)
+#
+#     number_of_blocks, avg_temp_exp, stdv_temp = block_averaging(cutoff, block_size=1000, data=temp_exp)
+#     number_of_blocks1, avg_potential_est, stdv_potential = block_averaging(cutoff, block_size=4000, data=pot_est)
+#     number_of_blocks2, avg_kin_est, stdv_kin = block_averaging(cutoff, block_size=4000, data=kin_est)
+#
+#     print("Mean classical Temperature:", avg_temp_exp, "+-", stdv_temp)
+#     print("Mean Potential_estimator:", avg_potential_est, "+-", stdv_potential)
+#     print("Mean Total (Kinetic)_estimator:", avg_kin_est, "+-", stdv_kin)
+#     print("Mean Total Energy Estimator:", avg_potential_est + avg_kin_est)
+#
+#     e_tot_array[i] = avg_kin_est
+#     e_tot_stdv_array[i] = np.sqrt(stdv_potential**2 + stdv_kin**2)
+#
+# print ("e array:", e_tot_array)
 #
 # figper = plt.figure()
 # plt.rcParams.update({'font.size': 13})
-# plt.plot(times, e_change, '.', label="Energy % Change", color="red")
-# plt.xlabel("time")
-# plt.ylabel("(E(t) - E0)*100 / E0  [%]")
-# plt.ylim([-0.1, 0.1])
+# plt.plot(beads_array, e_tot_array, '.', label="total energy", color="red")
+# plt.xlabel("beads")
+# plt.ylabel("Total Energy")
 # plt.legend()
 # plt.show()
+
+
+
+figenergy = plt.figure()
+plt.rcParams.update({'font.size': 13})
+plt.plot(times, e_tot, '.', label="Total Energy vs Time", color="black")
+plt.plot(times, kin, '.', label="Kinetic vs Time", color="red")
+plt.plot(times, potential, '.', label="Potential vs Time", color="blue")
+plt.xlabel("time")
+plt.ylabel("Energy")
+plt.legend()
+plt.show()
+#
+figper = plt.figure()
+plt.rcParams.update({'font.size': 13})
+plt.plot(times, e_change, '.', label="Energy % Change", color="red")
+plt.xlabel("time")
+plt.ylabel("(E(t) - E0)*100 / E0  [%]")
+plt.ylim([-0.1, 0.1])
+plt.legend()
+plt.show()
 
 
 # figtemp = plt.figure()
@@ -75,30 +77,30 @@ plt.show()
 # plt.legend()
 # plt.show()
 
-# figper = plt.figure()
-# plt.rcParams.update({'font.size': 13})
-# plt.plot(times, h_eff_change, '.', label="Effective Hamiltonian", color="red")
-# plt.xlabel("time")
-# plt.ylabel("(H_eff(t) - H_eff(0))*100 / H_eff(0) [%]")
-# plt.ylim([-0.1, 0.1])
-# plt.legend()
-# plt.show()
+figper = plt.figure()
+plt.rcParams.update({'font.size': 13})
+plt.plot(times, h_eff_change, '.', label="Effective Hamiltonian", color="red")
+plt.xlabel("time")
+plt.ylabel("(H_eff(t) - H_eff(0))*100 / H_eff(0) [%]")
+plt.ylim([-0.1, 0.1])
+plt.legend()
+plt.show()
 #
-# figpotest = plt.figure()
-# plt.rcParams.update({'font.size': 13})
-# plt.plot(steps, pot_est, '.', label="P_Estimator vs step", color="blue")
-# plt.xlabel("Steps")
-# plt.ylabel("Potential Estimator")
-# plt.legend()
-# plt.show()
-#
-# figpotest = plt.figure()
-# plt.rcParams.update({'font.size': 13})
-# plt.plot(steps, kin_est, '.', label="K_Estimator vs step", color="blue")
-# plt.xlabel("Steps")
-# plt.ylabel("Kinetic Estimator")
-# plt.legend()
-# plt.show()
+figpotest = plt.figure()
+plt.rcParams.update({'font.size': 13})
+plt.plot(steps, pot_est, '.', label="P_Estimator vs step", color="blue")
+plt.xlabel("Steps")
+plt.ylabel("Potential Estimator")
+plt.legend()
+plt.show()
+
+figpotest = plt.figure()
+plt.rcParams.update({'font.size': 13})
+plt.plot(steps, kin_est, '.', label="K_Estimator vs step", color="blue")
+plt.xlabel("Steps")
+plt.ylabel("Kinetic Estimator")
+plt.legend()
+plt.show()
 
 
 
